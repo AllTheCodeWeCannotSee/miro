ec3003c993c3d6bf97d60364d0c6b34b0264a228
AllTheCodeWeCannotSee <183735517@qq.com>
2024-02-26 11:39:00 +0800

    layer

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,99 +1,100 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton"
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { cn } from "@/lib/utils";
-import { Hint } from "@/components/hint";
-
+import Link from "next/link";
+import Image from "next/image";
+import { Menu } from "lucide-react";
 import { useQuery } from "convex/react";
 import { Poppins } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
-import { useRenameModal } from "@/store/use-rename-modal";
+
+import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
+import { api } from "@/convex/_generated/api";
 import { Actions } from "@/components/actions";
-import { Menu } from "lucide-react";
-
-const font = Poppins({
-    subsets: ["latin"],
-    weight: ["600"],
-})
-
-const TabSeperator = () => {
-    return (
-        <div className=" text-neutral-300 px-1.5">
-            |
-        </div>
-    )
-
-}
+import { Button } from "@/components/ui/button";
+import { Id } from "@/convex/_generated/dataModel";
+import { useRenameModal } from "@/store/use-rename-modal";
 
 interface InfoProps {
-    boardId: string
-}
+  boardId: string;
+};
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const TabSeparator = () => {
+  return (
+    <div className="text-neutral-300 px-1.5">
+      |
+    </div>
+  );
+};
 
 export const Info = ({
-    boardId
+  boardId,
 }: InfoProps) => {
-    const data = useQuery(api.board.get, {
-        id: boardId as Id<"boards">,
-    });
+  const { onOpen } = useRenameModal();
 
-    const { onOpen } = useRenameModal();
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<"boards">,
+  });
 
-    if (!data) return <InfoSkeleton />;
+  if (!data) return <InfoSkeleton />;
 
-
-    return (
-        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-            <Hint label="Go to boards" side="bottom" sideOffset={10}>
-
-                <Button asChild className="px-2" variant="board">
-                    <Link href="/">
-                        <Image
-                            src="/logo.svg"
-                            alt="Board logo"
-                            height={40}
-                            width={40}
-                        />
-                        <span className={cn("text-black font-semibold text-xl ml-2", font.className)}>
-                            Board
-                        </span>
-                    </Link>
-                </Button>
-            </Hint>
-            <TabSeperator />
-            <Hint label="Edit title" side="bottom" sideOffset={10}>
-
-                <Button
-                    className="text-base font-normal px-2"
-                    variant="board"
-                    onClick={() => onOpen(data._id, data.title)}
-                >
-                    {data.title}
-                </Button>
-            </Hint>
-            <TabSeperator />
-            <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
-                <div>
-                    <Hint label="Main menu" side="bottom" sideOffset={10}>
-                        <Button variant="board" size="icon">
-                            <Menu />
-                        </Button>
-                    </Hint>
-                </div>
-            </Actions>
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant="board" className="px-2">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Board logo"
+              height={40}
+              width={40}
+            />
+            <span className={cn(
+              "font-semibold text-xl ml-2 text-black",
+              font.className,
+            )}>
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          className="text-base font-normal px-2"
+          onClick={() => onOpen(data._id, data.title)}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions
+        id={data._id}
+        title={data.title}
+        side="bottom"
+        sideOffset={10}
+      >
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <Menu />
+            </Button>
+          </Hint>
         </div>
-    )
-}
+      </Actions>
+    </div>
+  );
+};
 
 export const InfoSkeleton = () => {
-    return (
-
-        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]">
-            <Skeleton className="w-full h-full bg-muted-400" />
-        </div>
-    )
-}
+  return (
+    <div 
+      className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"
+    />
+  );
+};
